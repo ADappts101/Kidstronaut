bda610d5b12cccbfcef9e137bfed5d8b70030646
ADappts <129835970+BearToRear@users.noreply.github.com>
2023-10-14 16:41:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function(){
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.height = 3%;
 let enemies = [];
 let score = 0;
 let gameOver = false;
