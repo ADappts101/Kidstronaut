cf031735c35fd1c096cad05cbd507788149edb24
ADappts <129835970+BearToRear@users.noreply.github.com>
2023-10-14 18:14:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -99,30 +99,26 @@ onGround(){
 }
 }
 
-  class Background {
-    constructor(gameWidth, gameHeight) {
-      this.gameWidth = gameWidth;
-      this.gameHeight = gameHeight;
-      this.image = document.querySelector('#backgroundImage');
-      this.x = 0;
-      this.y = this.gameHeight - this.height;
-      this.width = 2400;
-      this.height = 720;
-      this.speed = 7;
-    }
-
-    draw(context) {
-      context.drawImage(this.image, this.x, this.y, this.width, this.height);
-      context.drawImage(this.image, this.x, this.y - this.height, this.width, this.height);
-    }
-
-    update() {
-      this.y -= this.speed;
-      if (this.y < 0) {
-        this.y = this.gameHeight - this.height;
-      }
-    }
-  }
+class Background {
+constructor(gameWidth, gameHeight){
+this.gameWidth = gameWidth;
+this.gameHeight = gameHeight;
+this.image = document.querySelector('#backgroundImage');
+this.x = 0;
+this.y = 0;
+this.width = 2400;
+this.height = 720;
+this.speed = 7;
+}
+draw(context){
+    context.drawImage(this.image, this.x, this.y, this.width, this.height);
+    context.drawImage(this.image, this.x + this.width - this.speed, this.y, this.width, this.height);
+}
+update(){
+this.x -= this.speed;
+if (this.x < 0 - this.width) this.x = 0;
+}
+}
 
 class Enemy {
 constructor(gameWidth, gameHeight){
